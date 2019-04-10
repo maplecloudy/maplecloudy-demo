6defcf5ed0a75d47b14dc1639eda15eb13ed61c4
fgzhong <fguangzhong@163.com>
2019-04-10 10:54:41 +0800

    test-3

--- src/main/java/SparkSqlMain.java
+++ src/main/java/SparkSqlMain.java
@@ -1,3 +1,4 @@
+import com.google.gson.Gson;
 import com.maplecloudy.api.app.AppPod;
 import com.maplecloudy.app.MAppRunner;
 import com.maplecloudy.app.MAppTool;
@@ -28,14 +29,14 @@ public class SparkSqlMain implements MAppTool {
         String outPath = (String) appPod.getConfigMap().get("outPath");
         System.out.println(appPod.getConfigMap());
 //        System.getProperties().setProperty("HADOOP_USER_NAME",useName);
-        SparkConf scf = new SparkConf(true);
-        scf.set("hadoop.security.group.mapping", FakeUnixGroupsMapping.class.getName());
+        SparkConf scf = new SparkConf(true).setAppName("maplecloudy-spark-hive-app");
+//        scf.set("hadoop.security.group.mapping", FakeUnixGroupsMapping.class.getName());
         MAppUtils.appendHadoopConf2Spark(scf);
+        System.out.println(new Gson().toJson(scf.getAll()));
         SparkSession spark = SparkSession.builder().config(scf)
 //                .config("hive.metastore.uris", "thrift://dn5.ali.bjol.bigdata.udsp.com:9083")
 //                .config("hive.exec.local.scratchdir", "/tmp/maple")
                 .enableHiveSupport().getOrCreate();
-
         Dataset<Row> table = spark.sql(sql);
         table.write().format("com.databricks.spark.avro").mode(SaveMode.Overwrite)
                 .option("path",outPath)
